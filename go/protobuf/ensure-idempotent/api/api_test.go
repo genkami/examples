0bce7137ce69a6feb37d6198f39d48af5459e92f
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-07-17 02:01:09 +0900

    Ensure that non-readonly methods have idempotency_key

--- go/protobuf/ensure-idempotent/api/api_test.go
+++ go/protobuf/ensure-idempotent/api/api_test.go
@@ -1,7 +1,30 @@
 package api_test
 
-import "testing"
+import (
+	"testing"
 
-func TestExample(t *testing.T) {
-	// nop
+	pb "github.com/genkami/examples/go/protobuf/ensure-idempotent/api"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+func TestApi(t *testing.T) {
+	fileDesc := pb.File_api_api_proto
+	methodDescs := fileDesc.Services().ByName("Example").Methods()
+	for i := 0; i < methodDescs.Len(); i++ {
+		methodDesc := methodDescs.Get(i)
+		opts := methodDesc.Options().(*descriptorpb.MethodOptions)
+		var hasSideEffects bool
+		if opts.IdempotencyLevel == nil {
+			hasSideEffects = true
+		} else {
+			hasSideEffects = *opts.IdempotencyLevel != descriptorpb.MethodOptions_NO_SIDE_EFFECTS
+		}
+
+		if hasSideEffects {
+			idempotencyKeyDesc := methodDesc.Input().Fields().ByName("idempotency_key")
+			if idempotencyKeyDesc == nil {
+				t.Errorf("%s: non-readonly method must have idempotency_key", methodDesc.Name())
+			}
+		}
+	}
 }
